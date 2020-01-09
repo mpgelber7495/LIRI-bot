e18c3bd7b284ea62747ce69ead725d58073e2f0f
Michael Gelber <mpgelber7495@gmail.com>
2020-01-09 18:01:15 -0500

    Feature: foundational JS in liri.js file

--- liri.js
+++ liri.js
@@ -1,4 +1,9 @@
 require("dotenv").config();
 const axios = require("axios");
+const moment = require("moment");
+const dotEnv = require("dotenv");
 var keys = require("./keys.js");
+
+const [_, __, commandName, commandParam] = process.argv;
+
 var spotify = new Spotify(keys.spotify);
