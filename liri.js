17e674bde83f1c89a45f9d117603167cf3fc8802
Michael Gelber <mpgelber7495@gmail.com>
2020-01-12 15:28:59 -0500

    Feature: switched from if's to switch statement

--- liri.js
+++ liri.js
@@ -7,3 +7,23 @@ var keys = require("./keys.js");
 const [_, __, commandName, commandParam] = process.argv;
 
 var spotify = new Spotify(keys.spotify);
+switch (commandName) {
+  case "concert-this":
+    let artist = commandParam;
+    const queryURL =
+      "https://rest.bandsintown.com/artists/" +
+      artist +
+      "/events?app_id=codingbootcamp";
+
+    axios(queryURL.then());
+    break;
+  case "spotify-this-song":
+    let song = commandParam;
+    break;
+  case "movie-this":
+    let movie = commandParam;
+    break;
+  case "do-what-it-says":
+    break;
+  default:
+}
